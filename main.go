7ce38c43116791726dfb47975e501d8788a727ac
regibayoan <regi.bayoan@gmail.com>
2020-10-03 15:18:00 +0100

    2nd commit

--- main.go
+++ main.go
@@ -2,20 +2,13 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
 )
 
 func main() {
 	fmt.Println("Serving...")
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		fmt.Println("Inside / ")
-		d, _ := ioutil.ReadAll((r.Body))
-		fmt.Fprintf(rw, "Hello %s", d)
-	})
-	http.HandleFunc("/abc", func(http.ResponseWriter, *http.Request) {
-		fmt.Println("Inside /abc ")
-	})
+
+	hh := handlers.NewHello()
 	http.ListenAndServe(":9000", nil)
 
 }
